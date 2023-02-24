fcde11b493ba4c56549bc924b836a7908ac9c1ea
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-02-24 21:54:30 +0900

    add: invade prefabs & animation

--- Assets/Scripts/Invader.cs
+++ Assets/Scripts/Invader.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Invader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite[] animationSprites;
+
+    public float animationTime = 1.0f;
+
+    private SpriteRenderer SpriteRenderer;
+    private int animationFrame;
+
+    private void Awake()
     {
-        
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
     }
+
+    private void AnimateSprite()
+    {
+        animationFrame++;
+
+        if (animationFrame >= animationSprites.Length)
+        {
+            animationFrame = 0;
+        }
+
+        SpriteRenderer.sprite = animationSprites[animationFrame];
+    }
+
 }
